d8666d53df7a07e3f6c56791a6f3de3e374857ee
fravila08 <fr4v1l4@gmail.com>
2025-12-22 14:40:24 -0800

    clean up

--- curriculum/docs/phase-I/01-Js-Py-Intro/3-js-jest-intro/examples/myFaveNums.js
+++
@@ -1,7 +0,0 @@
-// myFaveNums.js
-
-const x = 1;
-const y = 2;
-const z = 3;
-
-module.exports = { x, y, z };
--- curriculum/docs/phase-I/01-Js-Py-Intro/4-python-pytest-intro/example/example.py
+++
@@ -1,3 +0,0 @@
-def add_two_numbers(num_one, num_two):
-    answer = num_one + num_two
-    return answer   
